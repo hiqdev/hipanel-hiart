eb03a2a3a19f291361d9f2f91b6ea2afc686512b
Andrii Vasyliev <sol@hiqdev.com>
2017-01-24 11:25:08 +0000

    emptied hisite config

--- src/config/hisite.php
+++ src/config/hisite.php
@@ -1,17 +1,3 @@
 <?php
 
-return [
-    'components' => [
-        'hiart' => [
-            'class' => \hipanel\hiart\Connection::class,
-            'config' => $params['hiart.config'],
-        ],
-    ],
-    'container' => [
-        'singletons' => [
-            \hipanel\hiart\ConnectionInterface::class => function () {
-                return Yii::$app->get('hiart');
-            },
-        ],
-    ],
-];
+return [];
