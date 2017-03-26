af87ee2a4c23c7e7c915aba16ef6ffcd7f680c92
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2017-03-26 19:31:00 +0300

    Added PHPUnit 6 compatibility

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -5,3 +5,17 @@ error_reporting(E_ALL & ~E_NOTICE);
 $bootstrap = __DIR__ . '/../src/_bootstrap.php';
 
 require_once file_exists($bootstrap) ? $bootstrap : __DIR__ . '/../vendor/autoload.php';
+
+/*
+ * Ensures compatibility with PHPUnit 6.x
+ */
+if (!class_exists('PHPUnit_Framework_Constraint') && class_exists('PHPUnit\Framework\Constraint\Constraint')) {
+    abstract class PHPUnit_Framework_Constraint extends \PHPUnit\Framework\Constraint\Constraint
+    {
+    }
+}
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    abstract class PHPUnit_Framework_TestCase extends \PHPUnit\Framework\TestCase
+    {
+    }
+}
