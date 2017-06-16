0eebed2a9e9f56afdb2a3b46fdc13ae6964cea4d
Andrii Vasyliev <sol@hiqdev.com>
2017-06-16 11:35:44 +0200

    trying

--- src/config/common.php
+++ src/config/common.php
@@ -8,4 +8,27 @@
  * @copyright Copyright (c) 2017, HiQDev (http://hiqdev.com/)
  */
 
-return [];
+return [
+    'components' => array_filter([
+        $params['hiart.dbname'] => array_filter([
+            'class'         => \hiqdev\hiart\demux\Connection::class,
+            'selector'      => function ($query) {
+                return in_array($query->from, ['bill', 'order'], true) ? 'billing-hiapi' : 'old-hiapi';
+            },
+        ]),
+        $params['old-hiapi.dbname'] => array_filter([
+            'class'         => \hipanel\hiart\old\Connection::class,
+            'requestClass'  => $params['hiart.requestClass'],
+            'name'          => $params['old-hiapi.dbname'],
+            'config'        => $params['hiart.config'],
+            'baseUri'       => $params['hiart.baseUri'],
+        ]),
+        $params['billing-hiapi.dbname'] => array_filter([
+            'class'         => \hipanel\hiart\hiapi\Connection::class,
+            'requestClass'  => $params['hiart.requestClass'],
+            'name'          => $params['billing-hiapi.dbname'],
+            'config'        => $params['billing-hiapi.config'],
+            'baseUri'       => $params['billing-hiapi.baseUri'],
+        ]),
+    ]),
+];
--- src/config/params.php
+++ src/config/params.php
@@ -9,6 +9,12 @@
  */
 
 return [
-    'hiart.class'   => \hipanel\hiart\Connection::class,
-    'hiart.baseUri' => 'http://hiapi.ahnames.com/',
+    'hiart.class'           => \hipanel\hiart\Connection::class,
+    'hiart.baseUri'         => 'http://hiapi.ahnames.com/',
+
+    'old-hiapi.dbname'      => 'old-hiapi',
+
+    'billing-hiapi.dbname'  => 'billing-hiapi',
+    'billing-hiapi.config'  => [],
+    'billing-hiapi.baseUri' => 'http://billing-hiapi.ahnames.com/',
 ];
